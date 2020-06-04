73834fc9bc63c800968acab9da0d6b5fe6839956
Enrique Manjavacas <enrique.manjavacas@gmail.com>
2020-06-04 20:43:22 +0200

    fix

--- retrieve/compare/soft_cosine.py
+++ retrieve/compare/soft_cosine.py
@@ -4,7 +4,7 @@ import numpy as np
 import tqdm
 
 
-def soft_cosine4(queries, index, M):
+def soft_cosine4(queries, index, S):
     """
     This function assumes that the order of vocabulary in the similarity matrix
     correspondes to the order of the vocabulary in the document representations
@@ -14,17 +14,17 @@ def soft_cosine4(queries, index, M):
 
     queries : np.array(n, vocab), n query docs in BOW format
     index : np.array(m, vocab), m indexed docs in BOW format
-    S : np.array(vocab, vocab), similarity matrix
+    S : np.array(vocab, vocab), similarity matrix (possibly raised to a power)
 
     Output
     ======
     sims : np.array(n, m), soft cosine similarities
     """
     sims = np.zeros((len(queries), len(index)))
-    MindexT = M @ index.T
-    den2 = np.sqrt(np.diag(index @ MindexT))
-    for idx, s in tqdm.tqdm(enumerate(queries), total=len(queries), desc='Soft cosine: '):
-        num = s[None, :] @ MindexT
-        den1 = np.sqrt(s @ M @ s)
+    SindexT = S @ index.T
+    den2 = np.sqrt(np.diag(index @ SindexT))
+    for idx, s in tqdm.tqdm(enumerate(queries), total=len(queries), desc='Soft cosine'):
+        num = s[None, :] @ SindexT
+        den1 = np.sqrt(s @ S @ s)
         sims[idx] = (num / ((np.ones(len(den2)) * den1) * den2))[0]
     return np.nan_to_num(sims, copy=False)
