2d13506eae965718b80ee5fed1a452a18d00a536
Enrique Manjavacas <enrique.manjavacas@gmail.com>
2020-09-02 12:53:35 +0200

    minor

--- retrieve/pipeline.py
+++ retrieve/pipeline.py
@@ -21,15 +21,15 @@ def pipeline(coll1, coll2=None,
              min_n=1, max_n=1, skip_k=0, sep='--',
              # Feature Selection
              criterion=None,
-             method='set-based', threshold=0, processes=-1, embs=None,
+             method='set-based', threshold=0, processes=-1, embs=None, chunk_size=5000,
              # Set-based
              # - SetSimilarity: similarity_fn
              #     ('containment', 'containment_min', 'jaccard')
              # VSM-based
              # - Tfidf: vocab, **sklearn,feature_extraction.text.TfidfVectorizer
              # Alignment-based
-             # - match, mismatch, open_gap, extend_gap, cutoff, factor
-             method_fit={},
+             # - match, mismatch, open_gap, extend_gap, cutoff, beta
+             method_params={},
              # Soft_cosine_params: cutoff, beta
              use_soft_cosine=False, soft_cosine_params={},
              # For Blast-style alignment
@@ -37,7 +37,7 @@ def pipeline(coll1, coll2=None,
              # return time stats
              return_stats=False):
 
-    colls = [coll for coll in [coll1, coll2] if coll]
+    colls = [coll for coll in [coll1, coll2] if coll is not None]
 
     if isinstance(stopwords, str):
         stopwords = utils.Stopwords(stopwords)
@@ -58,18 +58,18 @@ def pipeline(coll1, coll2=None,
         if method == 'set-based':
             coll1_feats = coll1.get_features(cast=set)
             coll2_feats = coll2.get_features(cast=set) if coll2 else coll1_feats
-            sims = SetSimilarity(threshold, **method_fit).get_similarities(
+            sims = SetSimilarity(threshold, **method_params).get_similarities(
                 coll1_feats, coll2_feats, processes=processes)
         elif method == 'vsm-based':
             coll1_feats = coll1.get_features()
             coll2_feats = coll2.get_features() if coll2 is not None else coll1_feats
-            tfidf = Tfidf(vocab, **method_fit).fit(coll1_feats + coll2_feats)
+            tfidf = Tfidf(vocab, **method_params).fit(coll1_feats + coll2_feats)
             if use_soft_cosine:
                 embs = require_embeddings(
                     embs, msg='soft cosine requires embeddings', vocab=vocab)
                 sims = tfidf.get_soft_cosine_similarities(
                     coll1_feats, coll2_feats, embs=embs,
-                    threshold=threshold, **soft_cosine_params)
+                    threshold=threshold, chunk_size=chunk_size, **soft_cosine_params)
             else:
                 sims = tfidf.get_similarities(
                     coll1_feats, coll2_feats, threshold=threshold)
@@ -77,19 +77,19 @@ def pipeline(coll1, coll2=None,
             if embs is not None:
                 embs = require_embeddings(embs, vocab=fsel.get_vocab())
                 scorer = create_embedding_scorer(
-                    embs,
-                    **{key: val for key, val in method_fit.items()
-                       if key in set(['match', 'mismatch', 'cutoff', 'factor'])})
+                    embs, chunk_size=chunk_size,
+                    **{key: val for key, val in method_params.items()
+                       if key in set(['match', 'mismatch', 'cutoff', 'beta'])})
             else:
                 scorer = ConstantScorer(
-                    **{key: val for key, val in method_fit.items()
+                    **{key: val for key, val in method_params.items()
                        if key in set(['match', 'mismatch'])})
             if precomputed_sims is not None:
                 print("Computing {} alignments...".format(precomputed_sims.nnz))
             sims = align_collections(
                 coll1, coll2,
                 S=precomputed_sims, field=None, processes=processes, scorer=scorer,
-                **{key: val for key, val in method_fit.items()
+                **{key: val for key, val in method_params.items()
                    if key in set(['extend_gap', 'open_gap'])})
         else:
             raise ValueError("Unknown method", method)
