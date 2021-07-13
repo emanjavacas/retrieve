26808f3b180c931f058b0857d1a451b0848364f6
Enrique Manjavacas <enrique.manjavacas@gmail.com>
2021-07-13 10:35:16 +0200

    fix app

--- src/retrieve/viz/app.py
+++ src/retrieve/viz/app.py
@@ -24,9 +24,11 @@ def extract_matching_words(doc1, doc2):
 
 
 class VisualizerApp:
-    def __init__(self, sims, coll1, coll2=None, sim_range=(None, None),
+    def __init__(self, sims, coll1, coll2=None, 
+                 sim_range=(None, None),
                  host='localhost', port=5000):
         """
+        Dot-plot visualization app
         """
         # data
         if coll2 is None:
