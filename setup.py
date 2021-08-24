c2b2eebbec469f61494079511b430ada8e637ec3
Enrique Manjavacas <enrique.manjavacas@gmail.com>
2021-08-24 15:36:49 +0200

    pumped up version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from Cython.Build import cythonize
 
 setup(
     name='text-reuse-retrieve',
-    version='0.1.11',
+    version='0.1.12',
     description='A Python package to perform Text Reuse Detection',
     url='https://github.com/emanjavacas/retrieve',
     author='Enrique Manjavacas',
